3c1bf6cb8a6171d6a212174d7081d634d7f7926a
swomfire <vucuongJim@gmail.com>
2019-02-27 17:09:53 +0700

    resolve change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import BasicExample from './BasicExample';
-import configureStore from './stores/Store';
+import configureStore from './stores';
 import { Provider } from 'react-redux';
 
 const store = configureStore();
--- src/stores/index.js
+++ src/stores/index.js
@@ -2,7 +2,6 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
 const initialState = {};
-// const thunkMiddleware = createThunkMiddleware();
 
 export default function configureStore() {
   const store = createStore(
