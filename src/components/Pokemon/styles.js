9d6bb3f2be1b25ddf6d8361c908ae6185ba118ff
swomfire <vucuongJim@gmail.com>
2019-03-05 10:03:47 +0700

    design for main page

---
+++ src/components/Pokemon/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PokemonCard = styled.div`
+    width: 100%;
+    height: 20%;
+    background: linear-gradient(10deg,#62d2fa 0,#043eff);;
+    box-sizing: border-box;
+    border: 2px solid #27278f;
+    padding: 10px;
+    text-align: center;
+
+    p{
+    color: white;
+    font-size: 1.5em;
+    font-weight: 600;
+    }
+`;
