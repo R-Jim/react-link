92ce88f2facde7541229f6513312d05886b4a969
swomfire <vucuongJim@gmail.com>
2019-03-05 15:57:14 +0700

    add saga to login

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-
 import login from './Login';
 import register from './Register';
 import account from './Account';
+
+export const REHYDRATION_COMPLETE = 'REHYDRATION_COMPLETE';
+
 
 export default combineReducers({
   login,
